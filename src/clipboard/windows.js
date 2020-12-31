eb2e5120dac0834f78b541fd948ab8bc80bfb9ca
Robert Gebauer <robert.gebauer@msg.group>
2020-12-31 08:36:18 +0100

    Switched copy to powershell command for Windows.

--- src/clipboard/windows.js
+++ src/clipboard/windows.js
@@ -1,5 +1,6 @@
 const childProcess = require('child_process')
 const execa = require('execa');
+const iconv = require("iconv-lite")
 
 const env = {
   ...process.env,
@@ -7,13 +8,20 @@ const env = {
 };
 
 module.exports = {
-  copy: text => childProcess.spawn('clip').stdin.end(text),
+  copy: (text) => {
+    options = {
+      "encoding": "UTF8",
+      "input": Buffer.from(text, "UTF-8")
+    }
+    
+    execa('powershell -command "$INPUT | Set-Clipboard "', {...options, env } )
+  },
   paste: () => {
     options = {
       "encoding": "UTF8"
     }
     
-    const process = execa('powershell -command "$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding ; Get-Clipboard"', {...options, env } )
+    const process = execa('powershell -command "[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding ; Get-Clipboard "', {...options, env } )
     
     return new Promise((resolve, reject) => {
       process.then(result => {
